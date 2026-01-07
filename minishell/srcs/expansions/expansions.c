87d2748dd5c4262aadc513e354d7514ed9c2e949
Mauro <mauroilija2003@gmail.com>
2026-01-08 00:04:54 +0100

    error expanding

--- minishell/srcs/expansions/expansions.c
+++ minishell/srcs/expansions/expansions.c
@@ -37,8 +37,152 @@
 
 static bool	is_valid(char c)
 {
-	if (ft_isalpha(c) || ft_isdigit(c))
+	if (ft_isalpha(c) || c == '_')
 		return (true);
 	return (false);
 }
 
+static	char *get_value(t_env *env, char *variable_name)
+{
+	t_env	*cur;
+	char	*value;
+
+	cur = env;
+	while (cur)
+	{
+		if (ft_strcmp(cur->key, variable_name))
+		{
+			value = getenv(variable_name); //OR MAYBE value = ft_strdup(cur->value)?
+			if (!value)
+			{
+				value = ft_strdup("");
+				if (!value)
+					return (NULL);
+			}
+		}
+		cur = cur->next;
+	}
+	return (value);
+}
+
+char	*expand_string(char *input, t_env *env, int exit_code)
+{
+	t_state	state;
+	int		number_of_quotes;
+	char	quote;
+	char	*result;
+	char	*value;
+
+	(void)exit_code;
+	result = NULL;
+	while (*input)
+	{
+		state = 0;
+		number_of_quotes = 0;
+		if (*input == '\'')
+		{
+			number_of_quotes = 1;
+			quote = *input;
+			while (*input)
+			{
+				if (*input == '\'')
+					number_of_quotes++;
+				input++;
+			}
+			if (*input == '\0' && number_of_quotes % 2 != 0)
+				return (printf("unclosed single quotes\n"), NULL);
+			state = 1;
+			//logic here is -> no expansion, just append full input to result
+			ft_strlcat(result, input, *input - quote); //allocate with malloc?
+			input++;
+		}
+		else if (*input == '\"')
+		{
+			quote = *input;
+			while (*input)
+			{
+				if (*input == '\"')
+					number_of_quotes++;
+				input++;
+			}
+			if (*input == '\0' && number_of_quotes % 2 != 0)
+				return (printf("unclosed double quotes\n"), NULL);
+			state = 2;
+			input = &quote;
+			while (*input)
+			{
+				if (*input == '$')
+				{
+					if (!is_valid(*(input + 1))) //if first variable name does not start with letter or _, error
+						return (NULL);
+					value = get_value(env, input);
+					if (!value)
+						return (NULL);
+					ft_strcat(result, value);
+					input += ft_strlen(value);
+				}
+				input++;
+			}
+		}
+		input++;
+	}
+	return (result);;
+}
+
+t_env *create_env(void)
+{
+    t_env *head = malloc(sizeof(t_env));
+    t_env *second = malloc(sizeof(t_env));
+
+    head->key = strdup("milija");
+    head->value = strdup("hello");
+    head->next = second;
+
+    second->key = strdup("user");
+    second->value = strdup("world");
+    second->next = NULL;
+
+    return head;
+}
+
+// Free the environment list
+void free_env(t_env *env)
+{
+    t_env *tmp;
+    while (env)
+    {
+        tmp = env;
+        env = env->next;
+        free(tmp->key);
+        free(tmp->value);
+        free(tmp);
+    }
+}
+
+int main(void)
+{
+    t_env *env = create_env();
+
+    char *tests[] = {
+        "$milija",
+        "No expansion here",
+        "Hello $user!",
+        "Mixed $milija-$user",
+        NULL
+    };
+
+    for (int i = 0; tests[i]; i++)
+    {
+        char *result = expand_string(tests[i], env, 0);
+        /*if (!result)
+            printf("Error expanding: %s\n", tests[i]);*/
+        //else
+        //{
+            printf("Input: \"%s\" --> Expanded: \"%s\"\n", tests[i], result);
+            free(result);  // Assuming expand_string allocates memory
+        //}
+    }
+
+    free_env(env);
+    return 0;
+}
